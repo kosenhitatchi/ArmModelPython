2d22f66886b43115d032504cd06ea630ed15327e
remi cambuzat <remi.cambuzat@gmail.com>
2016-01-08 15:08:28 +0100

    ajout fichier method_compilation_cython

---
+++ launch_cythonization.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./method_compilation_cython/clear_build.sh #Remove all file from compilation
+clear
+python method_compilation_cython/Cythonize_ArmModel.py build_ext --inplace #Lauch the compilation of the module ArmModel
+
+rm -r build #On supprime les fichiers temporaires (/build) issu de la compilations - @Todo : verifier si ont peux supprimer ce fichier
