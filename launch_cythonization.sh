9f45a3760d860441b3b8e976897fb3b500283b7e
remi cambuzat <remi.cambuzat@gmail.com>
2016-01-16 20:36:21 +0100

    update on the cythonisation script

--- launch_cythonization.sh
+++ launch_cythonization.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ./method_compilation_cython/clear_build.sh #Remove all file from compilation
-clear
-python method_compilation_cython/Cythonize_ArmModel.py build_ext --inplace #Lauch the compilation of the module ArmModel
+
+rm log_cythonisation.txt
+
+python method_compilation_cython/Cythonize_ArmModel.py build_ext --inplace >> log_cythonisation.txt #Lauch the compilation of the module ArmModel
 
 rm -r build #On supprime les fichiers temporaires (/build) issu de la compilations - @Todo : verifier si ont peux supprimer ce fichier
+
+echo "Cythonisation done : check log_cythonisation for more informations"
--- method_compilation_cython/Cythonize_ArmModel.py
+++ method_compilation_cython/Cythonize_ArmModel.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+
+import numpy #
 #
 # extensions = [
 #     Extension("MuscularActivation", ["MuscularActivation.pyx"]),
@@ -14,11 +16,21 @@ from Cython.Build import cythonize
     1) To launch the compilation run the command :
         python Cythonize_ArmModel.py build_ext --inplace
 '''
+
+extensions = [
+    Extension("*", #  '*' sinon genere un 'mon_extension.so' (shared object) a la racine
+              ["MotorControlModel/ArmModel/*.pyx"], #will all commpile all *.pyc in the directory
+              include_dirs=[numpy.get_include()], #include the numpyPy directory path
+              extra_compile_args=["-w"] # Option de comilation pour GCC : ici supprimer les warning
+    )
+    # D'autre Extension peuvent se trouver ici, pour des compilation avec d'autre parametres ou d'autre module a compiler
+    # ...
+
+]
+
 setup(
 	name = 'my cython app',
-	#  ext_modules = cythonize("DevCython/*.pyx") # Complile all *.pyx file in this folder
-	# ext_modules = cythonize(extensions)
-	ext_modules = cythonize("MotorControlModel/ArmModel/*.pyx")
+    ext_modules = cythonize(extensions)
 )
 
 '''
@@ -39,13 +51,30 @@ To check with module are inside the package, run at ../ArmModel :
             MusclesParameters
             MuscularActivation
 '''
+'''
+######### Documentation #####
+
+    Documentation compilation cython:
+        http://docs.cython.org/src/reference/compilation.html
+
+    Information la classe Extension (distutils documentation) :
+        https://docs.python.org/2/extending/building.html
+
+    ARgument de compilation pour gcc:
+        https://gcc.gnu.org/onlinedocs/gcc-3.4.4/gcc/Invoking-GCC.html#Invoking-GCC
+
+'''
 
 '''
 ###### TO DO ####
 
     @todo : Dependency Handling / Compile from the .py file
-        Look at pyximport and *.pyxdep
+        Look at pyximport and *.pyxdep :
+
+        If " import pyximport; pyximport.install() " is in toto.pyx, the compilation will be done for every new version of this file at startup ( no need of Cythonize_ArmModel.py anymore)
+        However this compilation won't handle complex case, like import of external c-libraries
 
         http://docs.cython.org/src/userguide/source_files_and_compilation.html#pyximport
+        http://docs.cython.org/src/reference/compilation.html#compiling-with-pyximport
 '''
 
